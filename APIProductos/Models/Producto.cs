ed96d406a04bb49927e19f365de4d1a355a71b2e
matiassromo <matiasromo10@hotmail.com>
2023-10-11 09:35:39 -0500

    Agregar archivos de proyecto.

---
+++ APIProductos/Models/Producto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace APIProductos.Models
+{
+    public class Producto
+    {
+        public int IdProducto { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int Cantidad { get; set; }
+
+        
+      
+    }
+
+}
